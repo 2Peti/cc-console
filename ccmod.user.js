ba781b8404f3d43562041ccaa2d268b64346b7e2
2Peti <44583946+2Peti@users.noreply.github.com>
2021-02-14 13:04:35 +0100

    Update ccmod.user.js

--- ccmod.user.js
+++ ccmod.user.js
@@ -3,6 +3,6 @@
 // @version      0.9
 // @description  Console Mod for Cookie Clicker
 // @author       Center-Z
-// @match        http://orteil.dashnet.org/cookieclicker/
+// @match        https://orteil.dashnet.org/cookieclicker/
 // ==/UserScript==
 fetch("https://raw.githubusercontent.com/2Peti/cc-console/main/src/source.js").then(res=>{if(res.ok) return res.text();}).then(result=>{eval(result);});
